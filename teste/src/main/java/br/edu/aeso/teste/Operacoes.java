d32f9bb244c3f795f811b10f8d56cab915965d46
fabricio <fabricio@192.168.0.101>
2017-05-15 23:28:34 -0300

    commit inicial do projeto teste

---
+++ teste/src/main/java/br/edu/aeso/teste/Operacoes.java
@@ -0,0 +1,21 @@
+package br.edu.aeso.teste;
+
+public class Operacoes {
+
+	private int num1;
+	private int num2;
+	
+	public Operacoes(int num1, int num2) {
+		this.num1 = num1;
+		this.num2 = num2;
+	}
+	
+	public int soma() {
+		return num1 + num2;
+	}
+	
+	public int subtracao() {
+		return num1 - num2;
+	}
+	
+}
---
+++ teste/src/test/java/br/edu/aeso/teste/OperacoesTest.java
@@ -0,0 +1,24 @@
+package br.edu.aeso.teste;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.edu.aeso.teste.Operacoes;
+
+public class OperacoesTest {
+
+	@Test
+	public void somaTest() {
+		Operacoes op = new Operacoes(3,3);
+		int ret = op.soma();
+		Assert.assertEquals(6, ret);
+	}
+	
+	@Test
+	public void subTest() {
+		Operacoes op = new Operacoes(5,3);
+		int ret = op.subtracao();
+		Assert.assertEquals(2, ret);
+	}
+
+}
